075918fb3908cab5691448394d90166fbb58d127
kdr250 <54400975+kdr250@users.noreply.github.com>
2024-07-31 11:37:35 +0900

    Hello WebGPU

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,7 +1,27 @@
+#include <webgpu/webgpu.h>
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World" << std::endl;
+    // create descriptor
+    WGPUInstanceDescriptor desc = {};
+    desc.nextInChain            = nullptr;
+
+    // create instance
+    WGPUInstance instance = wgpuCreateInstance(&desc);
+
+    // check whether there is actually an insatnce created
+    if (!instance)
+    {
+        std::cerr << "Could not initialize WebGPU!" << std::endl;
+        return 1;
+    }
+
+    // display the object (WGPUInstance is a simple pointer, it may be copied around without worrying about its size)
+    std::cout << "WGPU instance: " << instance << std::endl;
+
+    // clean up the WebGPU instance
+    wgpuInstanceRelease(instance);
+
     return 0;
 }
