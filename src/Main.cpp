6898fd683eaf52eb4c908b8a13ac7bb7d619ac37
kdr250 <54400975+kdr250@users.noreply.github.com>
2024-07-31 12:34:07 +0900

    Inspecting the adapter

--- src/Main.cpp
+++ src/Main.cpp
@@ -41,6 +41,64 @@ WGPUAdapter requestAdapterSync(WGPUInstance instance, const WGPURequestAdapterOp
     return userData.adapter;
 }
 
+void inspectAdapter(WGPUAdapter adapter)
+{
+    WGPUSupportedLimits supportedLimits = {};
+    supportedLimits.nextInChain         = nullptr;
+
+    bool success = wgpuAdapterGetLimits(adapter, &supportedLimits) == WGPUStatus_Success;
+
+    if (success)
+    {
+        std::cout << "Adapter limits:" << std::endl;
+        std::cout << " - maxTextureDimension1D: " << supportedLimits.limits.maxTextureDimension1D << std::endl;
+        std::cout << " - maxTextureDimension2D: " << supportedLimits.limits.maxTextureDimension2D << std::endl;
+        std::cout << " - maxTextureDimension3D: " << supportedLimits.limits.maxTextureDimension3D << std::endl;
+        std::cout << " - maxTextureArrayLayers: " << supportedLimits.limits.maxTextureArrayLayers << std::endl;
+    }
+
+    std::vector<WGPUFeatureName> features;
+    size_t featureCount = wgpuAdapterEnumerateFeatures(adapter, nullptr);
+    features.resize(featureCount);
+    wgpuAdapterEnumerateFeatures(adapter, features.data());
+
+    std::cout << "Adapter features:" << std::endl;
+    std::cout << std::hex;
+    for (auto f : features)
+    {
+        std::cout << " - 0x" << f << std::endl;
+    }
+    std::cout << std::dec;
+
+    WGPUAdapterProperties properties = {};
+    properties.nextInChain           = nullptr;
+    wgpuAdapterGetProperties(adapter, &properties);
+
+    std::cout << "Adapter properties:" << std::endl;
+    std::cout << " - vendorID: " << properties.vendorID << std::endl;
+    if (properties.vendorName)
+    {
+        std::cout << " - vendorName: " << properties.vendorName << std::endl;
+    }
+    if (properties.architecture)
+    {
+        std::cout << " - architecture: " << properties.architecture << std::endl;
+    }
+    std::cout << " - deviceID: " << properties.deviceID << std::endl;
+    if (properties.name)
+    {
+        std::cout << " - name: " << properties.name << std::endl;
+    }
+    if (properties.driverDescription)
+    {
+        std::cout << " - driverDescription: " << properties.driverDescription << std::endl;
+    }
+    std::cout << std::hex;
+    std::cout << " - adapterType: 0x" << properties.adapterType << std::endl;
+    std::cout << " - backendType: 0x" << properties.backendType << std::endl;
+    std::cout << std::dec;
+}
+
 int main()
 {
     // create descriptor
@@ -65,6 +123,9 @@ int main()
     adapterOpts.nextInChain               = nullptr;
     WGPUAdapter adapter                   = requestAdapterSync(instance, &adapterOpts);
     std::cout << "Got adapter: " << adapter << std::endl;
+
+    // display some information about adapter
+    inspectAdapter(adapter);
 
     // clean up the WebGPU instance
     wgpuInstanceRelease(instance);
