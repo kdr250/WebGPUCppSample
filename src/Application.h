bbf8013263489ee4bea7b4434ddb1e3499c678f3
kdr250 <54400975+kdr250@users.noreply.github.com>
2024-08-02 17:49:04 +0900

    Pimplパターンを適用

---
+++ src/Application.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <memory>
+
+namespace wgpu
+{
+    class TextureView;
+}  // namespace wgpu
+
+class Application
+{
+public:
+    Application();
+    ~Application();
+
+    // Initialize everything and return true if it went all right
+    bool Initialize();
+
+    // Uninitialize everything that was initialized
+    void Terminate();
+
+    // Draw a frame and handle events
+    void MainLoop();
+
+    // Return true as long as the main loop should keep on running
+    bool IsRunning();
+
+private:
+    struct AppData;
+
+    AppData* data;
+
+    wgpu::TextureView GetNextSurfaceTextureView();
+};
