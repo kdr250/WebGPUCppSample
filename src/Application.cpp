196cf4e3e0a6f396c67b8e4a18bfd0b55809d0a4
kdr250 <54400975+kdr250@users.noreply.github.com>
2024-08-08 13:26:51 +0900

    Generating mip-maps

--- src/Application.cpp
+++ src/Application.cpp
@@ -497,6 +497,7 @@ void Application::InitializePipeline()
         std::cerr << "Could not load texture!" << std::endl;
         assert(texture != nullptr);
     }
+    data->textureView = textureView;
 
     // Create a sampler
     wgpu::SamplerDescriptor samplerDesc;
@@ -725,23 +726,88 @@ MyUniforms Application::createUniforms()
 static void writeMipMaps(wgpu::Device device,
                          wgpu::Texture texture,
                          wgpu::Extent3D textureSize,
-                         [[maybe_unused]] uint32_t mipLevelCount,  // not used yet
+                         [[maybe_unused]] uint32_t mipLevelCount,
                          const unsigned char* pixelData)
 {
-    wgpu::ImageCopyTexture destination;
-    destination.texture  = texture;
-    destination.mipLevel = 0;
-    destination.origin   = {0, 0, 0};
-    destination.aspect   = wgpu::TextureAspect::All;
-
-    wgpu::TextureDataLayout source;
-    source.offset       = 0;
-    source.bytesPerRow  = 4 * textureSize.width;
-    source.rowsPerImage = textureSize.height;
-
     wgpu::Queue queue = device.getQueue();
-    queue.writeTexture(destination, pixelData, 4 * textureSize.width * textureSize.height, source, textureSize);
+
+    // Arguments telling which part of the texture we upload to
+    wgpu::ImageCopyTexture destination;
+    destination.texture = texture;
+    destination.origin  = {0, 0, 0};
+    destination.aspect  = wgpu::TextureAspect::All;
+
+    // Arguments telling how the C++ side pixel memory is laid out
+    wgpu::TextureDataLayout source;
+    source.offset = 0;
+
+    // Create image data
+    wgpu::Extent3D mipLevelSize = textureSize;
+    std::vector<unsigned char> previousLevelPixels;
+    wgpu::Extent3D previousMipLevelSize;
+    for (uint32_t level = 0; level < mipLevelCount; ++level)
+    {
+        // Pixel data for the current level
+        std::vector<unsigned char> pixels(4 * mipLevelSize.width * mipLevelSize.height);
+        if (level == 0)
+        {
+            // We cannot really avoid this copy since we need this
+            // in previousLevelPixels at the next iteration
+            memcpy(pixels.data(), pixelData, pixels.size());
+        }
+        else
+        {
+            // Create mip level data
+            for (uint32_t i = 0; i < mipLevelSize.width; ++i)
+            {
+                for (uint32_t j = 0; j < mipLevelSize.height; ++j)
+                {
+                    unsigned char* p = &pixels[4 * (j * mipLevelSize.width + i)];
+                    // Get the corresponding 4 pixels from the previous level
+                    unsigned char* p00 =
+                        &previousLevelPixels[4 * ((2 * j + 0) * previousMipLevelSize.width + (2 * i + 0))];
+                    unsigned char* p01 =
+                        &previousLevelPixels[4 * ((2 * j + 0) * previousMipLevelSize.width + (2 * i + 1))];
+                    unsigned char* p10 =
+                        &previousLevelPixels[4 * ((2 * j + 1) * previousMipLevelSize.width + (2 * i + 0))];
+                    unsigned char* p11 =
+                        &previousLevelPixels[4 * ((2 * j + 1) * previousMipLevelSize.width + (2 * i + 1))];
+                    // Average
+                    p[0] = (p00[0] + p01[0] + p10[0] + p11[0]) / 4;
+                    p[1] = (p00[1] + p01[1] + p10[1] + p11[1]) / 4;
+                    p[2] = (p00[2] + p01[2] + p10[2] + p11[2]) / 4;
+                    p[3] = (p00[3] + p01[3] + p10[3] + p11[3]) / 4;
+                }
+            }
+        }
+
+        // Upload data to the GPU texture
+        destination.mipLevel = level;
+        source.bytesPerRow   = 4 * mipLevelSize.width;
+        source.rowsPerImage  = mipLevelSize.height;
+        queue.writeTexture(destination, pixels.data(), pixels.size(), source, mipLevelSize);
+
+        previousLevelPixels  = std::move(pixels);
+        previousMipLevelSize = mipLevelSize;
+        mipLevelSize.width /= 2;
+        mipLevelSize.height /= 2;
+    }
+
     queue.release();
+}
+
+// Equivalent of std::bit_width that is available from C++20 onward
+uint32_t bit_width(uint32_t m)
+{
+    if (m == 0)
+        return 0;
+    else
+    {
+        uint32_t w = 0;
+        while (m >>= 1)
+            ++w;
+        return w;
+    }
 }
 
 wgpu::Texture loadTexture(const fs::path& path, wgpu::Device device, wgpu::TextureView* pTextureView)
@@ -754,9 +820,9 @@ wgpu::Texture loadTexture(const fs::path& path, wgpu::Device device, wgpu::Textu
     wgpu::TextureDescriptor textureDesc;
     textureDesc.dimension       = wgpu::TextureDimension::_2D;
     textureDesc.format          = wgpu::TextureFormat::RGBA8Unorm;  // by convention for bmp, png and jpg file
-    textureDesc.mipLevelCount   = 1;
-    textureDesc.sampleCount     = 1;
     textureDesc.size            = {(unsigned int)width, (unsigned int)height, 1};
+    textureDesc.mipLevelCount   = bit_width(std::max(textureDesc.size.width, textureDesc.size.height));
+    textureDesc.sampleCount     = 1;
     textureDesc.usage           = wgpu::TextureUsage::TextureBinding | wgpu::TextureUsage::CopyDst;
     textureDesc.viewFormatCount = 0;
     textureDesc.viewFormats     = nullptr;
